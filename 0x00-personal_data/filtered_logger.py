444672840a626f59a10a8833dde938468decea92
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-08-01 18:24:44 +0100

    Update script

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Function to filter data"""
 
-from typing import List
 import re
+import logging
 
 
 def filter_datum(fields, redaction, message, separator) -> str:
@@ -10,3 +10,21 @@ def filter_datum(fields, redaction, message, separator) -> str:
     for key in fields:
         message = re.sub(f'{key}=([^;\\s]+)', f'{key}={redaction}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Remove sensitive information"""
+        filtered = filter_datum(self.fields, self.REDACTION, super().format(record), self.SEPARATOR)
+        return filtered
