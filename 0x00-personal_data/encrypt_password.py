4f92a8a475859dd102270fc196697983f3803a97
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-08-02 17:08:26 +0100

    Add task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
 import bcrypt
-from typing import ByteString
 
 
-def hash_password(password: str) -> ByteString:
+def hash_password(password: str):
     """Hash a password"""
-    hashed: ByteString = bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt())
+    hashed: bytes = bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt())
     return hashed
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """Check the validity of the password"""
-    return(bcrypt.checkpw(bytes(password, 'utf-8'), hashed_password))
+    result: bool = bcrypt.checkpw(bytes(password, 'utf-8'), hashed_password)
+    return(result)
