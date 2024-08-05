f2c8c61b1ca6c700c508809ba3ee41702e804cd5
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-08-05 17:14:03 +0100

    Implement base64 decoding

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import base64
 from api.v1.auth.auth import Auth
+import re
 from typing import List
 
 
@@ -22,3 +24,33 @@ class BasicAuth(Auth):
         else:
             token: str = auth_array[1]
             return token
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """
+        Returns:
+            The decoded value of a Base64 string or None
+        """
+        if base64_authorization_header is None:
+            return None
+        elif not isinstance(base64_authorization_header, str):
+            return None
+        elif not self.validate_base64(base64_authorization_header):
+            return None
+        else:
+            decoded_str: bytes = base64.b64decode(base64_authorization_header)
+            return decoded_str.decode('utf-8')
+
+    def validate_base64(self, base64_string: str) -> bool:
+        """
+        Returns:
+            True if the string is a valid base64 string
+            and False if it is not
+        """
+        if (len(base64_string) % 4) != 0:
+            return False
+        elif re.match(r'^[a-zA-Z0-9+/=]*=?$', base64_string) is None:
+            return False
+        else:
+            return True
