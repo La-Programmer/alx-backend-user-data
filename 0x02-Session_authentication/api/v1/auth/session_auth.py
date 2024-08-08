bb6990b53694aee3068fb7c6fcdb7cab8e6e7be7
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-08-09 00:58:38 +0100

    Add get user_id froom session_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -12,6 +12,8 @@ class SessionAuth(Auth):
     def create_session(self, user_id: str = None) -> str:
         """
         Creates a SessionID for a userID
+        Return:
+            - Session ID (string) or None
         """
         if user_id is None:
             return None
@@ -21,3 +23,17 @@ class SessionAuth(Auth):
             session_id: str = str(uuid.uuid4())
             self.user_id_by_session_id[session_id] = user_id
             return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Get a user ID based on a Session ID
+        Return:
+            - User ID (string) or None
+        """
+        if session_id is None:
+            return None
+        elif not isinstance(session_id, str):
+            return None
+        else:
+            user_id = self.user_id_by_session_id.get(session_id)
+            return user_id
