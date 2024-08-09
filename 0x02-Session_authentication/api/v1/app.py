5f6dcf71fc0535ee4800a52eb9a8c319f5f43865
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-08-09 01:23:39 +0100

    Add sessionID check to API

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -31,14 +31,16 @@ def run_before_requests() -> str:
     excluded_paths = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/',
     ]
     if auth is None:
         pass
     elif auth.require_auth(request.path, excluded_paths) is False:
         pass
-    elif auth.authorization_header(request) is None:
-        abort(401)
+    elif auth.authorization_header(request):
+        if auth.session_cookie(request) is None:
+            abort(401)
     elif auth.current_user(request) is None:
         abort(403)
     request.current_user = auth.current_user(request)
