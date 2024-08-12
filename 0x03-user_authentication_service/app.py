9fdbc04d94bf1e95213723e9957452795e355214
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-08-12 15:38:03 +0100

    Add view to handle login functionality

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """ Flask App
 """
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -11,13 +11,13 @@ AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
-def home():
+def home() -> str:
     """Root endpoint"""
     return jsonify({"message": "Bienvenue"})
 
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
-def register_user():
+def register_user() -> str:
     """Handle user registration"""
     email: str = request.form.get('email')
     password: str = request.form.get('password')
@@ -30,6 +30,24 @@ def register_user():
         return jsonify({"email": email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """Handle user login"""
+    email: str = request.form.get('email')
+    password: str = request.form.get('password')
+    if email is None:
+        return jsonify({"error": "Email is missing"}), 400
+    elif password is None:
+        return jsonify({"error": "Password is missing"}), 400
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
