51ae65921276a14c0de9e90bd396ee07f3dcf7e6
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-08-12 14:42:48 +0100

    Add user registration functionality

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,9 @@
 """ Auth Module
 """
 
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 import bcrypt
 
 
@@ -16,3 +19,23 @@ def _hash_password(password: str) -> bytes:
     except Exception as e:
         print("Password hashing failed due to: ", str(e))
         return None
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Handle user registration
+        """
+        db = self._db
+        try:
+            user: User = db.find_user_by(email=email)
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound as e:
+            hashed_password: bytes = _hash_password(password)
+            new_user: User = db.add_user(email, hashed_password)
+            return new_user
