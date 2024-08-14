782a4d8dd87eedf7e6dd629b635e0d326582ee0d
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-08-14 11:10:25 +0100

    Fix task 4

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -12,14 +12,10 @@ import uuid
 def _hash_password(password: str) -> bytes:
     """Hash a password using bcrypt
     """
-    try:
-        hashed_password: bytes = bcrypt.hashpw(
-            bytes(password, 'utf-8'),
-            bcrypt.gensalt())
-        return hashed_password
-    except Exception as e:
-        print("Password hashing failed due to: ", str(e))
-        return None
+    hashed_password: bytes = bcrypt.hashpw(
+        bytes(password, 'utf-8'),
+        bcrypt.gensalt())
+    return hashed_password
 
 
 def _generate_uuid() -> str:
