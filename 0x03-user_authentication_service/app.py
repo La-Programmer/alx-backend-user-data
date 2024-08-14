e69b98bdc54dac535f519737d3f76f64c3c5b0d4
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-08-14 12:24:55 +0100

    Add user registration functionality

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,16 +2,35 @@
 """Flask App module
 """
 
-from flask import Flask, jsonify
+from auth import Auth
+from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/")
 def index():
     """Index route"""
     return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def register():
+    """Handle user registration
+    """
+    email: str = request.form.get('email')
+    password: str = request.form.get('password')
+    if not email and email.strip():
+        return jsonify({"error": "Email is required"}), 400
+    if not password and password.strip():
+        return jsonify({"error": "Password is required"}), 400
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": f"{email}", "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
