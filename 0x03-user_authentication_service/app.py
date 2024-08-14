19e03cbd49d0d531b4e88f4cec94c8c0a2cb4287
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-08-14 13:43:51 +0100

    Add login feature

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 """
 
 from auth import Auth
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 
 
 app = Flask(__name__)
@@ -31,6 +31,25 @@ def register():
         return jsonify({"email": f"{email}", "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login():
+    """Handle user login
+    """
+    email: str = request.form.get('email')
+    password: str = request.form.get('password')
+    if not email and email.strip():
+        return jsonify({"error": "Email is required"}), 400
+    if not password and password.strip():
+        return jsonify({"error": "Password is required"}), 400
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": f"{email}", "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response, 200
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
