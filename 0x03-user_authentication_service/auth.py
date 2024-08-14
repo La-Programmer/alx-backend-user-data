8effd7d15e43ff165b80e3ab4ca8f8d4d8be45de
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-08-14 12:04:52 +0100

    Add create user functionality

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,9 @@
 """ Auth Module
 """
 
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 import bcrypt
 
 
@@ -10,3 +13,25 @@ def _hash_password(password: str) -> bytes:
     password = bytes(password, 'utf-8')
     hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
     return hashed_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Handle user registration
+        """
+        db = self._db
+        if email is None or password is None:
+            return None
+        try:
+            user = db.find_user_by(email=email)
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            new_user: User = db.add_user(email, hashed_password)
+            return new_user
